6ae839e0f08833056684ac39700a1778cbeb88b8
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-06-06 23:34:23 -0400

    'Highlight' directive completed

--- 02_Directives/src/app/directive-highlight/highlight.directive.ts
+++ 02_Directives/src/app/directive-highlight/highlight.directive.ts
@@ -3,11 +3,38 @@
 // ElementRef injects into the directive's constructor so the code can access the DOM element.
 // Input allows data to flow from the binding expression into the directive.
 
-import { Directive, ElementRef, Input } from '@angular/core';
+import { Directive, ElementRef, HostListener,Input } from '@angular/core';
 
 @Directive({ selector: '[myHighlight]' })
 export class HighlightDirective {
-  constructor(el: ElementRef) {
-    el.nativeElement.style.backgroundColor = 'yellow';
+  constructor(private el: ElementRef) {
+    //el.nativeElement.style.backgroundColor = 'yellow';
   }
+
+  // pass value into the directive
+  @Input('myHighlight') highlightColor: string;
+
+  @HostListener('mouseenter') onMouseEnter() {
+    console.log('mouseEnter');
+    console.log(this.highlightColor);
+    // if color is null or not defined use red to hightlight
+    this.highlight(this.highlightColor || 'red');
+  }
+  // @HostListener('mousedown') onMouseDawn() {
+  //   console.log('mouseDawn');
+  //   this.highlight('red');
+  // }
+    @HostListener('click',['$event']) onMouseClick() {
+    console.log('mouseClicked');
+    this.highlight('green');
+  }
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight(null);
+  }
+
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+    this.el.nativeElement.style.color = 'pink';
+  }
+
 }
