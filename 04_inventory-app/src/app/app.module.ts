8eb468bb45acd59832d3e8892e0c72b6b3c05e1a
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-08-27 02:12:42 -0400

    product list component created

--- 04_inventory-app/src/app/app.module.ts
+++ 04_inventory-app/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductRowComponent } from './product-row/product-row.component';
+import { ProductImageComponent } from './product-image/product-image.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductRowComponent,
+    ProductImageComponent
   ],
   imports: [
     BrowserModule
