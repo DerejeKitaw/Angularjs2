c8c29d9533f96bc9ba7b1296811a0420a106c292
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-09-01 00:14:18 -0400

    bootstrap installed

--- 05_admin/src/environments/environment.prod.ts
+++ 05_admin/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCOulZwMIsRHUS0ENt0tCysrIfjqpUdi1Y",
+    authDomain: "mydemoproj-7ef9f.firebaseapp.com",
+    databaseURL: "https://mydemoproj-7ef9f.firebaseio.com",
+    projectId: "mydemoproj-7ef9f",
+    storageBucket: "mydemoproj-7ef9f.appspot.com",
+    messagingSenderId: "861414716387"
+  }
 };
